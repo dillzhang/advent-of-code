ec08616c9e11f8ce48aadabfbc888aa173c5979d
Dillon Zhang <d1998z@gmail.com>
2025-12-11 14:37:54 -0500

    Learn Z3 and finish Day 10

--- src/2025/day10/index.ts
+++ src/2025/day10/index.ts
@@ -1,91 +1,75 @@
-import { range } from "../../helpers/array";
+// import { range } from "../../helpers/array";
+const { init } = require("z3-solver");
 
 export default async (input: string[]) => {
-  return input
-    .map((line) => {
-      const components = line.split(" ");
-      // const indicators = components
-      //   .at(0)!
-      //   .slice(1, -1)
-      //   .split("")
-      //   .map((i) => {
-      //     return i === "#" ? 1 : 0;
-      //   });
-      const joltages = components
-        .at(-1)!
-        .slice(1, -1)
-        .split(",")
-        .map((i) => parseInt(i));
-      const buttons = components.slice(1, -1).map((button) => {
-        return button
+  return (
+    await Promise.all(
+      input.map(async (line, lineNumber) => {
+        const components = line.split(" ");
+        // const indicators = components
+        //   .at(0)!
+        //   .slice(1, -1)
+        //   .split("")
+        //   .map((i) => {
+        //     return i === "#" ? 1 : 0;
+        //   });
+        const joltages = components
+          .at(-1)!
           .slice(1, -1)
           .split(",")
           .map((i) => parseInt(i));
-      });
 
-      // return range(2 ** buttons.length)
-      //   .map((i) => {
-      //     if (i === 0) {
-      //       return Number.MAX_SAFE_INTEGER;
-      //     }
-      //     const binaryString = i
-      //       .toString(2)
-      //       .padStart(buttons.length, "0")
-      //       .split("");
+        const { Context } = await init();
+        const { Optimize, Int } = new Context("test");
+        const solver = new Optimize();
 
-      //     const result = binaryString
-      //       .reduce((acc, bit, index) => {
-      //         if (bit === "1") {
-      //           buttons[index].forEach((toggle) => {
-      //             acc[toggle] += 1;
-      //           });
-      //         }
-      //         return acc;
-      //       }, Array(indicators.length).fill(0))
-      //       .every((toggle, i) => toggle % 2 === indicators[i]);
+        const buttons = components.slice(1, -1).map((button) => {
+          const presses = Int.const(button);
+          solver.add(presses.ge(0));
 
-      //     if (result) {
-      //       return binaryString.filter((i) => i === "1").length;
-      //     }
-      //     return Number.MAX_SAFE_INTEGER;
-      //   })
-      //   .reduce((acc, cur) => Math.min(acc, cur), Number.MAX_SAFE_INTEGER);
+          return {
+            effected: new Set(
+              button
+                .slice(1, -1)
+                .split(",")
+                .map((i) => parseInt(i))
+            ),
+            presses,
+          };
+        });
 
-      const seen = new Set();
-      const frontier = [{ presses: 0, jolts: [...joltages] }];
+        joltages.forEach((joltage, joltIndex) => {
+          const sum = buttons
+            .filter(({ effected }) => {
+              return effected.has(joltIndex);
+            })
+            .map(({ presses }) => presses)
+            .reduce((acc, cur) => {
+              return acc.add(cur);
+            });
+          solver.add(sum.eq(joltage));
+        });
 
-      while (frontier.length > 0) {
-        const { presses: prevPresses, jolts } = frontier.shift()!;
-        if (jolts.every((jolt) => jolt === 0)) {
-          return prevPresses;
+        const sum = buttons
+          .map(({ presses }) => presses)
+          .reduce((acc, cur) => {
+            return acc.add(cur);
+          });
+        solver.minimize(sum);
+
+        const isSat = await solver.check();
+        if (isSat === "sat") {
+          const model = solver.model();
+          const pressCount = buttons.map(({ presses }) => {
+            return parseInt(model.get(presses).toString(), 10);
+          });
+          return pressCount.reduce((acc, cur) => acc + cur, 0);
         }
 
-        const presses = prevPresses + 1;
-
-        buttons.forEach((button) => {
-          const newJolts = button.reduce(
-            (acc, i) => {
-              acc[i] -= 1;
-              return acc;
-            },
-            [...jolts]
-          );
-
-          const joltString = newJolts.join(",");
-          if (seen.has(joltString)) {
-            return;
-          }
-          seen.add(joltString);
-
-          if (newJolts.every((jolt) => jolt >= 0)) {
-            frontier.push({ presses, jolts: newJolts });
-          }
-        });
-      }
-      throw new Error("No Solution Found");
-    })
-    .reduce((acc, cur) => {
-      // console.log(cur);
-      return acc + cur;
-    }, 0);
+        throw new Error("No Solution Found");
+      })
+    )
+  ).reduce((acc, cur) => {
+    return acc + cur;
+  }, 0);
 };
